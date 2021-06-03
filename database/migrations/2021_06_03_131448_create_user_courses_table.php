7356cee5f154604d462dddbbeaaae57006016c32
Gabi Piñeiro <gabispineiro@gmail.com>
2021-06-03 15:37:23 +0200

    criacion de la tabla pivot

---
+++ database/migrations/2021_06_03_131448_create_user_courses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_courses', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->uniqid();
+            $table->foreign('user_id')->references('user_id')->on('users');
+            $table->integer('course_id')->uniqid();
+            $table->foreign('course_id')->references('course_id')->on('courses');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_courses');
+    }
+}
