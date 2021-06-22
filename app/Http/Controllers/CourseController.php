53ea67a27ec67aba9f3170bea033765c42abaabf
Andres Patiño <andresp199519@gmail.com>
2021-06-22 14:34:42 +0200

    Se puso al condición para que el botón de suscribirse a un cruso quede inhabilitado si la fecha del curso ya pasó.

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -20,7 +20,7 @@ class CourseController extends Controller
     public function index()
     {
         $sliderCourses = Course::where('favorite', true)->orderByDesc('created_at')->take(6)->get();
-        $courses =Course::all()->sortBy('date');
+        $courses =Course::all()->sortByDesc('date');
 
         return view('welcome', ['sliderCourses'=>$sliderCourses, 'courses'=>$courses]);
 
@@ -35,7 +35,7 @@ class CourseController extends Controller
     public function home()
     {
         $sliderCourses = Course::where('favorite', true)->orderByDesc('created_at')->take(6)->get();
-        $courses =Course::all()->sortBy('date');
+        $courses =Course::all()->sortByDesc('date');
         return view('home', ['sliderCourses'=>$sliderCourses, 'courses'=>$courses]);
     }
     
@@ -69,6 +69,12 @@ class CourseController extends Controller
         $user->courses()->detach($course_id);
         return redirect()->route('myCourses');
     
+    }
+
+
+    public function deactivateCourse()
+    {
+
     }
 
     public function sendEmail()
